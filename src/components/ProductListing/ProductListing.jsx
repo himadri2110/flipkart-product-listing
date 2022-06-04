7ed2bf0350dbe661238f5eaf02abbeb2ca365bf8
himadri2110 <18it.himadri.shah@gmail.com>
2022-06-04 11:51:44 +0530

    styling: grid responsiveness

--- src/components/ProductListing/ProductListing.jsx
+++ src/components/ProductListing/ProductListing.jsx
@@ -24,7 +24,7 @@ export const ProductListing = () => {
             Showing {finalFilteredProducts.length} of {data.length} products
           </div>
 
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
             {finalFilteredProducts?.map((product) => (
               <ProductCard product={product} key={product.id} />
             ))}
